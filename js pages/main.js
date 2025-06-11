4e887d537e3a9997f350967bf6aabfd0185448cc
Shimaa Ashraf <mohedashraf2004@gmail.com>
2025-06-11 22:46:28 +0300

    updatejs

--- js pages/main.js
+++ js pages/main.js
@@ -1,5 +1,8 @@
 const toggleModeBtn = document.getElementById("toggle-mode");
 const body = document.querySelector("body");
+const menuBtn = document.querySelector(".menu-toggle-button");
+const menu = document.querySelector(".nav-list");
+const toTopBtn = document.querySelector(".to-top-btn");
 
 
 toggleModeBtn.addEventListener("click", () => {
@@ -7,5 +10,25 @@ toggleModeBtn.addEventListener("click", () => {
     body.classList.toggle("dark");
 });
 
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("active");
+});
+document.addEventListener("click", (e) => {
+    if(!menu.contains(e.target) && !menuBtn.contains(e.target) && !toTopBtn.contains(e.target)) {
+        menu.classList.remove("active");
+    }
+});
+
+window.addEventListener("scroll", () => {
+    (scrollY >= 100) ? toTopBtn.classList.add("show") : toTopBtn.classList.remove("show");
+});
+
+toTopBtn.addEventListener("click", () => {
+    scroll({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+});
 
 
